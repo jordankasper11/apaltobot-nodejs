cda2c5c18f4210901f3faea6b515c89f9c07565d
Jordan Kasper <jordankasper11@gmail.com>
2021-11-02 00:21:47 -0400

    Removed space after asterisk for server members

--- src/discord/discord-bot.ts
+++ src/discord/discord-bot.ts
@@ -326,7 +326,7 @@ export class DiscordBot {
 
         if (pilotUsers.length) {
             const progressIntervals = 20;
-            const usernameColumn = getColumn('  User', getMaxLength(pilotUsers.map(u => getUsername(u.user, u.guildMember))) + 2, columnSeparator);
+            const usernameColumn = getColumn(' User', getMaxLength(pilotUsers.map(u => getUsername(u.user, u.guildMember))) + 1, columnSeparator);
             const callsignColumn = getColumn('ID', getMaxLength(pilotUsers.map(u => u.pilot?.callsign)), columnSeparator);
             const aircraftColumn = getColumn('A/C', getMaxLength(pilotUsers.map(u => u.pilot?.flightPlan?.aircraftShort)), columnSeparator);
             const departureColumn = getColumn('DEP', 4, 1 + progressIntervals + 1);
@@ -346,7 +346,7 @@ export class DiscordBot {
             for (const pilotUser of pilotUsers) {
                 let row = '';
 
-                row += addValue(usernameColumn, (pilotUser.guildMember ? '* ' : '  ') + getUsername(pilotUser.user, pilotUser.guildMember));
+                row += addValue(usernameColumn, (pilotUser.guildMember ? '*' : ' ') + getUsername(pilotUser.user, pilotUser.guildMember));
                 row += addValue(callsignColumn, pilotUser.pilot?.callsign);
                 row += addValue(aircraftColumn, pilotUser.pilot?.flightPlan?.aircraftShort);
 
@@ -397,7 +397,7 @@ export class DiscordBot {
             .sort((x, y) => getUsername(x.user, x.guildMember).localeCompare(getUsername(y.user, y.guildMember)));
 
         if (controllerUsers.length) {
-            const usernameColumn = getColumn('  User', getMaxLength(controllerUsers.map(u => getUsername(u.user, u.guildMember))) + 2, columnSeparator);
+            const usernameColumn = getColumn(' User', getMaxLength(controllerUsers.map(u => getUsername(u.user, u.guildMember))) + 1, columnSeparator);
             const callsignColumn = getColumn('ID', getMaxLength(controllerUsers.map(u => u.controller?.callsign)), columnSeparator);
             const onlineColumn = getColumn('Online', 6, 0);
 
@@ -413,7 +413,7 @@ export class DiscordBot {
             for (const controllerUser of controllerUsers) {
                 let row = '';
 
-                row += addValue(usernameColumn, (controllerUser.guildMember ? '* ' : '  ') + getUsername(controllerUser.user, controllerUser.guildMember));
+                row += addValue(usernameColumn, (controllerUser.guildMember ? '*' : ' ') + getUsername(controllerUser.user, controllerUser.guildMember));
                 row += addValue(callsignColumn, controllerUser.controller?.callsign);
                 row += addValue(onlineColumn, controllerUser?.controller?.onlineSince ? DateTime.utc().diff(DateTime.fromJSDate(controllerUser.controller?.onlineSince)).toFormat('hh:mm') : '');
 
